a5d34f8490ce2f0884d951020ed838a7a6a0ec19
Restored [.-.] <86575196+NoNFake@users.noreply.github.com>
2022-11-08 16:35:53 +0200

    Add files via upload

--- mouse.py
+++ mouse.py
@@ -1,10 +1,15 @@
 #pip install pyautogui
 import pyautogui as pag
+
+
+#if u have error  use this '  pip install pyautogui   '
+
+
 import random
 import time
 
 while True:
-    x = random.randint(0,1280);
-    y = random.randint(0,1024);
-    pag.moveTo(x,y,0.9)
+    x = random.randint(0,1280); #x cordinate
+    y = random.randint(0,1024); #y cordinate
+    pag.moveTo(x,y,0.9) # move 'X' and 'Y', 0.9 - this speed..
     time.sleep(0.1)
---
+++ start.sh
@@ -0,0 +1,13 @@
+echo "                  Hi, now I ranning your cursor...yep"
+sleep 1
+echo "                    this is very simle python code"
+sleep 1
+cat mouse.py
+sleep 4
+echo "                                   heh,yes"
+sleep 1
+echo "                         DON'T THOUCH THE MOUSE!"
+sleep 2
+echo "                           ranning 'mouse.py'"
+echo "                   for stop this, press 'CTRL + C'"
+python3 mouse.py;
